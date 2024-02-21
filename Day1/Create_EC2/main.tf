6332b8526aa32bac7b31e135117e4b983a8eeda9
Saif Ali <saifali1035@gmail.com>
2024-02-21 23:37:57 +0530

    Added Key, Added subnet

--- Day1/Create_EC2/main.tf
+++ Day1/Create_EC2/main.tf
@@ -5,7 +5,8 @@ provider "aws" {
 resource "aws_instance" "Example" {
     ami = "ami-0e670eb768a5fc3d4" #Amazon Linux 2023 AMI 2023.3.20240219.0 x86_64 HVM kernel-6.1
     instance_type = "t2.micro"  
-
+    subnet_id = "subnet-008698a4ad5513812" #Deafult Subnet for Mumbai -1b
+    key_name = "Project1"
     tags = {
       Name = "Example Server"
     }
